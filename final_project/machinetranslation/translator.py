f0396bc3fe9d8552357ebf47aea268afbb1129eb
algarviz <agarciavizcaino@gmail.com>
2022-10-04 09:55:34 +0200

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,43 @@
+"""
+A simple translation app that can translate from English to French
+and from French to English using IBM's Watson IA.
+"""
+import os
+# import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version= '2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def english_to_french(english_text):
+    """
+    TRANSLATION FROM ENGLISH TO FRENCH
+    """
+    translation = language_translator.translate(
+    text = english_text,
+    model_id = 'en-fr').get_result()
+    french_text=translation['translations'][0]['translation']
+    return french_text
+
+# TRANSLATE FROM FRENCH TO ENGLISH:
+def french_to_english(french_text):
+    """
+    TRANSLATION FROM FRENCH TO ENGLISH
+    """
+    translation = language_translator.translate(
+    text = french_text,
+    model_id = 'fr-en').get_result()
+    english_text=translation['translations'][0]['translation']
+    return english_text
